8cab51b6e5ef60af797fab44a337c923f5d54de9
Simon Gravelle <simon.gravelle@live.fr>
2025-05-02 23:34:28 +0200

    improved testing

--- tests/test_twomolecules.py
+++ tests/test_twomolecules.py
@@ -13,6 +13,7 @@ import MDAnalysis as mda
 import numpy as np
 
 from nmrdfrommd import NMRD
+from nmrdfrommd.utilities import compute_rij, cartesian_to_spherical
 
 @pytest.fixture
 def twomol_universe():
@@ -56,3 +57,98 @@ def test_shape_array(twomol_universe):
     assert nmrd_aniso.R1.shape == (n_fft,)
     assert nmrd_aniso.R2.shape == (n_fft,)
     assert nmrd_aniso.gij.T[0].shape == (3,)
+
+def test_distance(twomol_universe):
+    """Assert that the calculated distance is correct."""
+    u, group_H2O = twomol_universe
+
+    nmrd_pbc = NMRD(
+        u=u,
+        atom_group=group_H2O,
+        pbc=True)
+    nmrd_pbc.run_analysis()
+
+    rij = compute_rij(
+        nmrd_pbc.position_i,
+        nmrd_pbc.position_j,
+        nmrd_pbc.box,
+        nmrd_pbc.pbc)
+
+    assert np.isclose(rij[0], -8.0)
+    assert np.isclose(rij[1], 0.0)
+    assert np.isclose(rij[2], 0.0)
+
+    nmrd_nopbc = NMRD(
+        u=u,
+        atom_group=group_H2O,
+        pbc=False)
+    nmrd_nopbc.run_analysis()
+
+    rij = compute_rij(
+        nmrd_nopbc.position_i,
+        nmrd_nopbc.position_j,
+        nmrd_nopbc.box,
+        nmrd_nopbc.pbc)
+
+    assert np.isclose(rij[0], 10.0)
+    assert np.isclose(rij[1], 0.0)
+    assert np.isclose(rij[2], 0.0)
+
+def test_distance(twomol_universe):
+    """Assert that the calculated spherical functions are correct."""
+    u, group_H2O = twomol_universe
+
+    nmrd_pbc = NMRD(
+        u=u,
+        atom_group=group_H2O,
+        pbc=True)
+    nmrd_pbc.run_analysis()
+
+    rij = compute_rij(
+        nmrd_pbc.position_i,
+        nmrd_pbc.position_j,
+        nmrd_pbc.box,
+        nmrd_pbc.pbc)
+    r, theta, phi = cartesian_to_spherical(rij)
+
+    assert np.isclose(r[0], 8.0)
+    assert np.isclose(theta[0], np.pi/2)
+    assert np.isclose(phi[0], np.pi)
+
+    nmrd_nopbc = NMRD(
+        u=u,
+        atom_group=group_H2O,
+        pbc=False)
+    nmrd_nopbc.run_analysis()
+
+    rij = compute_rij(
+        nmrd_nopbc.position_i,
+        nmrd_nopbc.position_j,
+        nmrd_nopbc.box,
+        nmrd_nopbc.pbc)
+    r, theta, phi = cartesian_to_spherical(rij)
+
+    assert np.isclose(r[0], 10.0)
+    assert np.isclose(theta[0], np.pi/2)
+    assert np.isclose(phi[0], 0)
+
+if False:
+    def test_correlation(twomol_universe):
+        """Assert that the calculated correlation is correct."""
+        u, group_H2O = twomol_universe
+
+        nmrd_pbc = NMRD(
+            u=u,
+            atom_group=group_H2O,
+            pbc=True)
+        nmrd_pbc.run_analysis()
+
+        assert np.float32(np.round(nmrd_pbc.gij[0][0], 7)) == np.float32(np.round(1/8**6, 7))
+
+        nmrd_nopbc = NMRD(
+            u=u,
+            atom_group=group_H2O,
+            pbc=False)
+        nmrd_nopbc.run_analysis()
+
+        assert np.float32(np.round(nmrd_nopbc.gij[0][0], 7)) == np.float32(np.round(1/10**6, 7))
